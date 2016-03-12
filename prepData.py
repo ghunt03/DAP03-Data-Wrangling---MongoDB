15ae87d3ecf34e1f0503b9806cd35ddb6b1d686f
Gareth Hunt <ghunt03@gmail.com>
2016-03-12 12:52:23 +1100

    Updated file paths for external data files

--- prepData.py
+++ prepData.py
@@ -1,14 +1,3 @@
-"""
-Your task in this exercise has two steps:
-
-- audit the OSMFILE and change the variable 'mapping' to reflect the changes needed to fix 
-    the unexpected street types to the appropriate ones in the expected list.
-    You have to add mappings only for the actual problems you find in this OSMFILE,
-    not a generalized solution, since that may and will depend on the particular area you are auditing.
-- write the update_name function, to actually fix the street name.
-    The function takes a string with street name as an argument and should return the fixed name
-    We have provided a simple test so that you see what exactly is expected
-"""
 import xml.etree.cElementTree as ET
 from collections import defaultdict
 import re
@@ -18,8 +7,8 @@ import numpy as np
 import codecs
 import json
 
-CITYDATAFILE = "AU.txt"
-AMENITYDATAFILE = "amenity_map.csv"
+CITYDATAFILE = "data_suburbs.txt"
+AMENITYDATAFILE = "data_amenity_map.csv"
 CREATED = [ "version", "changeset", "timestamp", "user", "uid"]
 OSMFILE = "map.osm"
 UPDATE = True
